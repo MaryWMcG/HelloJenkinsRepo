1875bd3d9f0c694ea750af2351bb513a46d5258d
L00113835 <mtwmcg1@gmail.com>
2018-12-02 20:20:02 +0000

    Hello world test

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,12 @@
+import java.io.PrintStream;
+public class HelloWorld {
+    public static void main (String [] argus){
+         print(System.out);
+
+    }
+
+        public static void print(PrintStream out){
+            out.println("Hello World today");
+        }
+
+}//close class
